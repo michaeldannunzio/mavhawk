a97ef25587c5590fb524952af8f68e98dd555c21
${Michael} <michael.dannunzio3@gmail.com>
2019-04-04 09:52:36 -0500

    thanks node for not supporting rpi

---
+++ app/build/precache-manifest.10729d5f7a9b1095029ce92cda1dfb28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8dac8542af416e3d778",
+    "url": "/static/js/main.a7b2f27f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab2b40779dfeb4f432b9",
+    "url": "/static/js/2.5495941f.chunk.js"
+  },
+  {
+    "revision": "0c4bad9bf79d838a35225e5735a74f02",
+    "url": "/static/media/logo.0c4bad9b.png"
+  },
+  {
+    "revision": "87d7c79690990eb744f66578fb19ea2f",
+    "url": "/index.html"
+  }
+];
